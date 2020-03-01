f7f47daaf16c32857bbd3d23dd779bc19d907108
mo-masker <1050251827@qq.com>
2020-03-01 22:31:26 +0800

    tab栏和首页轮播图

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-//app.js
+import request from "/utils/request.js"
 App({
   onLaunch: function () {
-    
+    // 设置基准路径
+    request.defaults.baseURL = "https://api-hmugo-web.itheima.net/api/public/v1"
   },
 
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,20 @@
-
+import request from "../../utils/request.js"
 Page({
-  
+  data: {
+    // 轮播图的数据
+    banners: []
+  },
+  onLoad() {
+    // 请求轮播图的数据
+    request({
+      url: "/home/swiperdata"
+    }).then(res => {
+      // console.log(res)
+      const { message } = res.data
+      this.setData({
+        banners: message
+      })
+
+    })
+  }
 })
