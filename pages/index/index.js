a8ffc186182fb805e777d257fa9b732f23d2f661
mo-masker <1050251827@qq.com>
2020-03-02 16:01:00 +0800

    导航菜单和楼层分类

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,11 @@ import request from "../../utils/request.js"
 Page({
   data: {
     // 轮播图的数据
-    banners: []
+    banners: [],
+    // 导航菜单的数据
+    menus: [],
+    // 楼层的数据
+    floors:[]
   },
   onLoad() {
     // 请求轮播图的数据
@@ -14,7 +18,42 @@ Page({
       this.setData({
         banners: message
       })
+    })
 
+    // 请求中间导航的接口
+    request({
+      url: "/home/catitems"
+    }).then(res => {
+      // console.log(res)
+      const {
+        message
+      } = res.data;
+
+      // 循环添加跳转链接
+      const newData = message.map((v, i) => {
+        // 判断更改导航菜单的跳转路径
+        // 更改分类的跳转路径
+        if (i === 0) {
+          v.url = "/pages/category/index"
+        }
+        // 然后把更改后的数据返回，再保存到munus中
+        return v;
+      })
+      this.setData({
+        menus: newData
+      })
+    })
+
+    // 请求楼层分类的数据
+    request({
+      url:"/home/floordata"
+    }).then(res=>{
+      // console.log(res)
+      const { message } = res.data;
+
+      this.setData({
+        floors:message
+      })
     })
   }
 })
