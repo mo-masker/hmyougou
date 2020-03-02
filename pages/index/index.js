c7c002eadd6e8aa5b8ddda1bec373018c3b3011f
mo-masker <1050251827@qq.com>
2020-03-02 22:37:18 +0800

    回到顶部功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,9 @@ Page({
     // 导航菜单的数据
     menus: [],
     // 楼层的数据
-    floors:[]
+    floors:[],
+    // 页面是否滚动
+    isShowTop:""
   },
   onLoad() {
     // 请求轮播图的数据
@@ -54,6 +56,37 @@ Page({
       this.setData({
         floors:message
       })
+    })
+  },
+
+  // 回到顶部点击事件
+  handleTotop(){
+    wx.pageScrollTo({
+      scrollTop: 0,
+      duration: 300
+    })
+  },
+
+  // 页面滚动触发事件的处理函数
+  // 监听用户滑动页面事件
+  onPageScroll(e){
+    console.log(e)
+    const { scrollTop } = e;
+    // 保存当前的显示状态
+    let isShow = this.data.isShowTop;
+
+    // 判断如果滚动高度大于100，显示回到顶部的按钮
+    if (scrollTop > 100){
+      isShow = true
+    }else {
+      isShow = false;
+    }
+
+    // 避免频繁的操作setData，所以如果下面两个值是相同就没必要再赋值了
+    if(isShow === isShowTop) return;
+
+    this.setData({
+      isShowTop:isShow
     })
   }
 })
