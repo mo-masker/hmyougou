a9af85a0af3b13ece3337fb572d8f76ae1efd7f8
mo-masker <1050251827@qq.com>
2020-03-03 23:04:33 +0800

    分类页面内容布局，左侧菜单点击高亮

--- pages/category/index.js
+++ pages/category/index.js
@@ -5,6 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 记录当前点击的菜单
+    current:""
 
   },
 
@@ -15,52 +17,15 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 菜单项的点击事件
+  handleClick(e){
+    // console.log(e)
+    // 获取索引值
+    const {index} = e.currentTarget.dataset;
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 保存当前点击的索引值
+    this.setData({
+      current:index
+    })
   }
 })
