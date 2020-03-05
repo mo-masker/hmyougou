90adb66776796fa9cfb47cb27773e65709480134
mo-masker <1050251827@qq.com>
2020-03-06 02:12:29 +0800

    加载下一页数据

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -11,7 +11,44 @@ Page({
     // 商品数据
     goods: [],
     // 是否还有数据
-    hasMore: true
+    hasMore: true,
+    // 页码数
+    pagenum: 1
+  },
+
+  // 封装请求的方法 方便调用
+  getGoods() {
+    setTimeout(v => {
+      // 传入参数请求商品列表数据
+      request({
+        url: "/goods/search",
+        data: {
+          // 参数 关键字
+          query: this.data.keyword,
+          // 页码
+          pagenum: this.data.pagenum,
+          // 数据条数
+          pagesize: 10
+        }
+      }).then(res => {
+        // console.log(res)
+        const {
+          message
+        } = res.data;
+
+        // 遍历修改goods下面的价格
+        const goods = message.goods.map(v => {
+          // 给价格保留两个小数点
+          v.goods_price = Number(v.goods_price).toFixed(2);
+          return v;
+        })
+
+        this.setData({
+          // 合并原来的商品列表和请求回来的列表  解构赋值
+          goods:[...this.data.goods,...goods]
+        })
+      })
+    }, 1000)
   },
 
   /**
@@ -27,71 +64,7 @@ Page({
         keyword
       }),
 
-      setTimeout(v => {
-        // 传入参数请求商品列表数据
-        request({
-          url: "/goods/search",
-          data: {
-            // 参数 关键字
-            query: this.data.keyword,
-            // 页码
-            pagenum: 1,
-            // 数据条数
-            pagesize: 10
-          }
-        }).then(res => {
-          // console.log(res)
-          const {
-            message
-          } = res.data;
-
-          // 遍历修改goods下面的价格
-          const goods = message.goods.map(v => {
-            // 给价格保留两个小数点
-            v.goods_price = Number(v.goods_price).toFixed(2);
-            return v;
-          })
-
-          this.setData({
-            goods
-          })
-        })
-      },3000)
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
+      this.getGoods();
 
   },
 
@@ -99,13 +72,12 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+    // 页数+1
+    this.setData({
+      pagenum: this.data.pagenum + 1
+    })
+    this.getGoods();
   }
+
+
 })
