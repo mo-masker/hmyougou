79f387bd1b7881d92d2e029bcd929604dadfc410
mo-masker <1050251827@qq.com>
2020-03-06 01:06:41 +0800

    请求商品列表渲染商品数据

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,11 +1,15 @@
-// pages/goods_list/index.js
+import request from "../../utils/request.js"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    keyword: ''
+    // 保存输入的搜索参数
+    keyword: '',
+    // 商品数据
+    goods:[]
   },
 
   /**
@@ -17,7 +21,34 @@ Page({
 
     this.setData({
       keyword
-    })
+    }),
+
+      // 传入参数请求商品列表数据
+      request({
+      url:"/goods/search",
+      data:{
+        // 参数 关键字
+        query:this.data.keyword,
+        // 页码
+        pagenum:1,
+        // 数据条数
+        pagesize:10
+      }
+      }).then(res=>{
+        // console.log(res)
+        const { message } = res.data;
+
+        // 遍历修改goods下面的价格
+        const goods = message.goods.map(v => {
+          // 给价格保留两个小数点
+          v.goods_price = Number(v.goods_price).toFixed(2);
+          return v;
+        })
+
+        this.setData({
+          goods
+        })
+      })
   },
 
   /**
