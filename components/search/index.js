6656becb3d0066207fdde8c8d50d0070b66f1335
mo-masker <1050251827@qq.com>
2020-03-07 02:26:26 +0800

    自定义组件的扩展--外部样式类

--- components/search/index.js
+++ components/search/index.js
@@ -4,8 +4,14 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    // 组件可以传递的值
+    keyword:{
+      type:String,
+      value: "搜索" // 这个是默认值，类似于vue里面的default
+    }
   },
+  // 外部扩展的样式  声明接收的样式
+  externalClasses: ['my-class'],
 
   /**
    * 组件的初始数据
