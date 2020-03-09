8e3a324b097c975251e256e29df0a1b301885334
mo-masker <1050251827@qq.com>
2020-03-10 01:58:41 +0800

    搜索页布局，input点击事件

---
+++ pages/search/index.js
@@ -0,0 +1,31 @@
+// pages/search/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 输入框的值
+    inputValue: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  // 监听输入框的输入事件
+  handleInput(e) {
+    // console.log(e.detail);
+    const {
+      value
+    } = e.detail;
+
+    this.setData({
+      inputValue: value
+    })
+  }
+
+})
