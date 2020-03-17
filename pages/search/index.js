76270d2bca7aaba5ecacedd3e76939acd810c3ac
mo-masker <1050251827@qq.com>
2020-03-18 01:59:02 +0800

    点击取消按钮清空输入框的值-控制请求接口函数的执行频率（防抖和节流）

--- pages/search/index.js
+++ pages/search/index.js
@@ -7,7 +7,12 @@ Page({
   data: {
     // 输入框的值
     inputValue: '',
-    recommend:''
+    // 上一次输入框输入请求的值
+    lastValue: '',
+    // 推荐的内容列表
+    recommend: [],
+    // 加载的状态
+    loading: false
   },
 
   /**
@@ -28,21 +33,57 @@ Page({
       inputValue: value
     })
     // value为空就返回  有值继续请求
-    if(!value) return;
-
-    // 请求搜索建议
-    request({
-      url:"/goods/qsearch",
-      data:{
-        query:value
-      }
-    }).then(res=>{
-      // console.log(res)
-      const {message} = res.data;
-
+    if (!value) {
+      // 把搜索建议的数组清空
       this.setData({
-        recommend:message
+        recommend: []
       })
+      return;
+    }
+
+    this.getRecommend();
+  },
+
+  // 封装的请求搜索建议方法
+  getRecommend() {
+    if (this.data.loading == false) {
+      this.setData({
+        loading: true,
+        lastValue: this.data.inputValue
+      })
+
+      // 请求搜索建议
+      request({
+        url: "/goods/qsearch",
+        data: {
+          query: this.data.inputValue
+        }
+      }).then(res => {
+        // console.log(res)
+        const {
+          message
+        } = res.data;
+
+        // 把搜索返回的数据保存到recommend
+        this.setData({
+          recommend: message,
+          // 数据加载完成后 更改加载状态
+          loading: false
+        })
+        // 判断inputValue的值是最新的，如果不是再重新请求
+        if (this.data.inputValue !== this.data.lastValue) {
+          this.getRecommend();
+        }
+      })
+
+    }
+
+  },
+  // 取消按钮的点击事件
+  handleCancel() {
+    this.setData({
+      inputValue: "",
+      recommend: ''
     })
   }
 
