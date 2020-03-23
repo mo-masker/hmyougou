3859136b4ccc28b2e098fd2ad394a06e2ad8335d
mo-masker <1050251827@qq.com>
2020-03-24 04:55:56 +0800

    获取收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,62 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 收货地址
+    address: {}
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 获取收货地址
+  handleGetAddress() {
+    wx.chooseAddress({
+      success: (res) => {
 
-  },
+        this.setData({
+          address: {
+            // 姓名
+            name: res.userName,
+            // 电话
+            tel: res.telNumber,
+            // 地址
+            detailAddress: res.provinceName + res.cityName + res.countyName + res.detailInfo
+          }
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+        })
+      }
+    })
   }
+
+
 })
